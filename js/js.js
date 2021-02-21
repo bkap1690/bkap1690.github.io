c56107e9656fbe2de5346b9684ab4f91c90ac4dd
34362012 <48579856+bkap31690@users.noreply.github.com>
2021-02-20 23:19:42 -0500

    SPOT App & other changes

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-  $("#secondary-nav").hide();
+  $('#secondary-nav').hide();
 
   // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
+  $('a').on('click', function (event) {
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== '') {
       // Prevent default anchor click behavior
       event.preventDefault();
 
@@ -13,7 +13,8 @@ $(document).ready(function () {
 
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $("html, body").animate({
+      $('html, body').animate(
+        {
           scrollTop: $(hash).offset().top,
         },
         800,
@@ -33,7 +34,7 @@ var TxtRotate = function (el, toRotate, period) {
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 1000;
-  this.txt = "";
+  this.txt = '';
   this.tick();
   this.isDeleting = false;
 };
@@ -48,7 +49,7 @@ TxtRotate.prototype.tick = function () {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
   var that = this;
   var delta = 300 - Math.random() * 100;
@@ -60,7 +61,7 @@ TxtRotate.prototype.tick = function () {
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === "") {
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
@@ -72,49 +73,48 @@ TxtRotate.prototype.tick = function () {
 };
 
 window.onload = function () {
-  var elements = document.getElementsByClassName("txt-rotate");
+  var elements = document.getElementsByClassName('txt-rotate');
   for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-rotate");
-    var period = elements[i].getAttribute("data-period");
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
   // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
+  var css = document.createElement('style');
+  css.type = 'text/css';
   css.innerHTML =
-    ".txt-rotate > .wrap { border-right: 0.08em solid transparent }";
+    '.txt-rotate > .wrap { border-right: 0.08em solid transparent }';
   document.body.appendChild(css);
 };
 
 $(function () {
   $(window).scroll(function (e) {
     if ($(this).scrollTop() > 800) {
-      $("#secondary-nav").fadeIn("slow");
+      $('#secondary-nav').fadeIn('slow');
     } else {
-      $("#secondary-nav").fadeOut("fast");
+      $('#secondary-nav').fadeOut('fast');
     }
   });
 });
 
-
-
 //Cross-browser smooth-scroll JS:
 
-$('a[href^="#"]').on("click", function (e) {
+$('a[href^="#"]').on('click', function (e) {
   e.preventDefault();
 
   var target = this.hash;
   var $target = $(target);
 
-  $("html, body")
+  $('html, body')
     .stop()
-    .animate({
+    .animate(
+      {
         scrollTop: $target.offset().top,
       },
       700,
-      "swing",
+      'swing',
       function () {
         window.location.hash = target;
       }
@@ -124,18 +124,16 @@ $('a[href^="#"]').on("click", function (e) {
 //Check to see if the window is top if not then display button
 $(window).scroll(function () {
   if ($(this).scrollTop() > 300) {
-    $("#scroll-to-top").fadeIn();
+    $('#scroll-to-top').fadeIn();
   } else {
-    $("#scroll-to-top").fadeOut();
+    $('#scroll-to-top').fadeOut();
   }
 });
 
-
-
-
 //Click event to scroll to top
-$("#scroll-to-top").click(function () {
-  $("html, body").animate({
+$('#scroll-to-top').click(function () {
+  $('html, body').animate(
+    {
       scrollTop: 0,
     },
     600
@@ -152,14 +150,14 @@ $(document).ready(function () {
     return $(window).scrollTop();
   };
 
-  if ("max" in document.createElement("progress")) {
-    var progressBar = $("progress");
+  if ('max' in document.createElement('progress')) {
+    var progressBar = $('progress');
 
     progressBar.attr({
       max: getMax(),
     });
 
-    $(document).on("scroll", function () {
+    $(document).on('scroll', function () {
       progressBar.attr({
         value: getValue(),
       });
@@ -172,7 +170,7 @@ $(document).ready(function () {
       });
     });
   } else {
-    var progressBar = $(".progress-bar"),
+    var progressBar = $('.progress-bar'),
       max = getMax(),
       value,
       width;
@@ -180,7 +178,7 @@ $(document).ready(function () {
     var getWidth = function () {
       value = getValue();
       width = (value / max) * 100;
-      width = width + "%";
+      width = width + '%';
       return width;
     };
 
@@ -190,8 +188,8 @@ $(document).ready(function () {
       });
     };
 
-    $(document).on("scroll", setWidth);
-    $(window).on("resize", function () {
+    $(document).on('scroll', setWidth);
+    $(window).on('resize', function () {
       max = getMax();
       setWidth();
     });
@@ -199,47 +197,13 @@ $(document).ready(function () {
 });
 
 $(window).scroll(function () {
-  $("nav").toggleClass("scrolled", $(this).scrollTop() > 100);
+  $('nav').toggleClass('scrolled', $(this).scrollTop() > 100);
 });
 
-// Some random colors
-// const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
-
-// const numBalls = 50;
-// const balls = [];
-
-// for (let i = 0; i < numBalls; i++) {
-//   let ball = document.createElement("div");
-//   ball.classList.add("ball");
-//   ball.style.background = colors[Math.floor(Math.random() * colors.length)];
-//   ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
-//   ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
-//   ball.style.transform = `scale(${Math.random()})`;
-//   ball.style.width = `${Math.random()}em`;
-//   ball.style.height = ball.style.width;
-  
-//   balls.push(ball);
-//   document.body.append(ball);
-// }
-
-// Keyframes
-// balls.forEach((el, i, ra) => {
-//   let to = {
-//     x: Math.random() * (i % 2 === 0 ? -11 : 11),
-//     y: Math.random() * 12
-//   };
-
-//   let anim = el.animate(
-//     [
-//       { transform: "translate(0, 0)" },
-//       { transform: `translate(${to.x}rem, ${to.y}rem)` }
-//     ],
-//     {
-//       duration: (Math.random() + 1) * 2000, // random duration
-//       direction: "alternate",
-//       fill: "both",
-//       iterations: Infinity,
-//       easing: "ease-in-out"
-//     }
-//   );
-// });
+// Listen to images after DOM content is fully loaded
+document.addEventListener('DOMContentLoaded', function () {
+  new Zooming({
+    // options...
+    zIndex: '4000',
+  }).listen('.img-zoomable');
+});
